ac71669cadea41a0173d60236a8721837ab9573d
DrewE <andrew.engelstein@gmail.com>
2019-06-24 16:56:34 -0700

    LC 48 - Rotate matrix 90degrees in place

---
+++ LC-48.js
@@ -0,0 +1,29 @@
+/*
+48. Rotate Image
+Medium
+
+You are given an n x n 2D matrix representing an image.
+
+Rotate the image by 90 degrees (clockwise).
+
+Note:
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+*/
+
+const transpose = matrix => {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < i; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+};
+
+const reverseRows = matrix => {
+  matrix.forEach(row => row.reverse());
+};
+
+var rotate = function(matrix) {
+  transpose(matrix);
+  reverseRows(matrix);
+};
